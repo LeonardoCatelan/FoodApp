8eb2c7b338f1f4d777eb7ff395e60a1092695048
leonardo_catelan <leonardo_catelan@dellteam.com>
2021-05-13 09:46:54 -0500

    project foundation added

---
+++ Food.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Food.DataAccess
+{
+    public class Class1
+    {
+    }
+}
